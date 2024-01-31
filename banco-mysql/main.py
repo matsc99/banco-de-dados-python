0deae492c61888bdb8a21e2ac243ce8366e672bf
matsc99 <mateus.sousa.cruz@hotmail.com>
2024-01-31 16:18:24 -0300

    manipulando dados

--- banco-mysql/main.py
+++ banco-mysql/main.py
@@ -12,14 +12,38 @@ connection = pymysql.connect(
     database=os.environ['MYSQL_DATABASE'],
 )
 
+TABLE_NAME = 'customers'
+
 with connection:
     with connection.cursor() as cursor:
         cursor.execute(
-            'CREATE TABLE IF NOT EXISTS customers ('
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}  ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
             ')'
         )
-        print(cursor)
+
+        # LIMPA A TABELA
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    connection.commit()
+
+    # Manipulando dados a partir daqui
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) ' 
+            'VALUES ' 
+            '(%s, %s) '
+        )
+
+        data = (
+            ('Mateus', 24),
+            ('Fernanda', 26),
+        )
+
+        cursor.executemany(sql, data)
+
+    connection.commit()
